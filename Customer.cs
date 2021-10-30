e23860cf015691d9c234034becdfed8588464a53
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-10-30 07:12:35 +0100

    After Customer Class

--- Customer.cs
+++ Customer.cs
@@ -9,7 +9,19 @@ namespace JYRAX.BL
     public class Customer
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+
+        private string _lastName;
+        public string LastName 
+        {
+            get
+            {
+                return _lastName;
+            }
+            set 
+            {
+                _lastName = value;
+            }
+        }
         public string Email { get; set; }
         public int CustomerId { get; private set; }
         public string FullName
@@ -29,5 +41,30 @@ namespace JYRAX.BL
             }
         }
         public static int instanceCount { get; set; }
+
+
+        //Methods
+        //Retrieve one Customer
+        public Customer Retrieve(int customerId)
+        {
+            //To retrieve a customer from ID
+            return new Customer();
+        }
+
+        //Retrieve All Customer
+        public List<Customer> Retrieve()
+        {
+            //To retrieve a customer from ID
+            return new List<Customer>();
+        }
+        
+        //Validate the Data;
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrEmpty(LastName)) isValid = false;
+            if (string.IsNullOrEmpty(Email)) isValid = false;
+            return isValid;
+        }
     }
 }
