bd65b38221d9af149e0fce664a46d75857fc6476
Amos Sunmbo Alagbe <sunboy005@gmail.com>
2021-10-30 07:40:38 +0100

    Other Classes & their Properties

---
+++ OrderProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JYRAX.BL
+{
+    class OrderProduct
+    {
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+        public string ProductOrdered { get; set; }
+        public int PurchasedPrice { get; set; }
+    }
+}
